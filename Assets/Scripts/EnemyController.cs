bba2fd4ed01bc295d1b32a05cc678272e91b2b00
maradw <maria.mendoza@tecsup.edu.pe>
2024-05-21 22:48:11 -0500

    Graphs

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
+
+public class EnemyController : MonoBehaviour
+{
+    public float currentTime = 0f;
+    public float timer;
+    public GameObject objective;
+    public Vector2 speedReference;
+    public int energy;
+
+    [SerializeField] private TextMeshProUGUI lifeState;
+    void Update()
+    {
+        
+        if (energy > 0)
+        {
+            transform.position = Vector2.SmoothDamp(transform.position, objective.transform.position, ref speedReference, 0.5f);
+        }
+        else if(energy <= 0)
+        {
+            transform.position = Vector2.zero;
+            currentTime = currentTime + Time.deltaTime;
+            if (currentTime >= timer)
+            {
+
+                energy = 30;
+                currentTime = 0;
+            }
+        }
+
+        SetLifeText();
+    }
+    public void SetLifeText()
+    {
+        lifeState.text = " Energy: " + energy;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Node") 
+        {
+            objective = collision.gameObject.GetComponent<NodeController>().SelecRandomAdjancent().gameObject;
+            int weight = objective.GetComponent<NodeController>().GetNodeWeight();
+            RestLife(weight);
+        }
+    }
+    public void RestLife(int weight)
+    {
+        energy = energy - weight;
+
+    }
+}
